df8017ab839564666c537e84dc795f23dea0ad3b
eeo_lqy <qingyi.luan@eeoa.com>
2017-11-22 10:51:05 +0800

    test

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -12,7 +12,7 @@ const Detail = () => import(/* webpackChunkName: "register" */ '@/templates/deta
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     // {
     //   path: '/',
